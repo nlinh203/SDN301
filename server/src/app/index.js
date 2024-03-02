fccf9c57172c9971ad801bec601cad649e3489f8
Ngoc Linh <linhntnhe171088@fpt.edu.vn>
2024-03-02 12:22:36 +0700

    linh

--- server/src/app/index.js
+++ server/src/app/index.js
@@ -2,4 +2,10 @@ export * from './authController';
 export * from './userController';
 export * from './courseController';
 export * from './lessonController';
-
+export * from './questionController';
+export * from './postController';
+export * from './commentController';
+export * from './courseReviewController';
+export * from './notifyController';
+export * from './templateController';
+export * from './logController';
