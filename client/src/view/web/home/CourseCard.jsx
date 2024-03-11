e931bb806ad0b62f0d71d56847dce001531219c9
Ngoc Linh <linhntnhe171088@fpt.edu.vn>
2024-03-11 12:52:49 +0700

    linh

--- client/src/view/web/home/CourseCard.jsx
+++ client/src/view/web/home/CourseCard.jsx
@@ -3,8 +3,3 @@ import { FaCrown } from 'react-icons/fa';
 import { formatNumber } from '@utils';
 import { Button, Link, Rating } from '@components/uiCore';
 
-const CourseCard = ({ item }) => {
-  const [isHovered, setIsHovered] = useState(false);
-  const isPro = item.price > 0
-
-}
